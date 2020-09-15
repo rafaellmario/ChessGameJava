8169ac65e3ab989cb803fcf1d9bf76c3bf3d1ac0
rafaellmario <rafaellmario@hotmail.com>
2020-09-15 20:50:35 -0300

    Project Creation

---
+++ src/application/MainClass.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.Locale;
+
+public class MainClass {
+  public static void main(String[] args) {
+	Locale.setDefault(Locale.US);
+	
+	System.out.println("Chess Game ucing Java!");
+	
+  }
+}
