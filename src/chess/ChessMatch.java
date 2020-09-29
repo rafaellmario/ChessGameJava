8ab6e9ea59b418db9b2f7c518d080c64d54a8752
rafaellmario <rafaellmario@hotmail.com>
2020-09-29 07:44:37 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
   private void validateSourcePosition(Position position) {
 	  if(!this.board.thereIsAPiece(position))
 		  throw new ChessException("There is no piece on source position!");
+	  
+	  if(!this.board.piece(position).isThereAnyPossibleMove())
+		  throw new ChessException("There is no possible moves for the chosen pice");
   }
   
   
