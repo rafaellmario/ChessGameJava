57b9ed85381bfa87dd03850526c3533186f6672c
rafaellmario <rafaellmario@hotmail.com>
2020-12-07 09:34:53 -0300

    Fix - Treatment to promotion

--- src/application/MainClass.java
+++ src/application/MainClass.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -49,16 +50,26 @@ public class MainClass {
 			
 			if(chessMatch.getPromoted()!=null) {
 				System.out.print("Enter piece for promotion(B/N/R/Q):");
-				String type = sc.nextLine();
-				chessMatch.replacePromotedPiece(type);
-			}
+				String type = sc.nextLine().toUpperCase();
 				
+				while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+					System.out.print("Invalid argument! Enter piece for promotion(B/N/R/Q):");
+					type = sc.nextLine().toUpperCase();
+				}
+				
+				chessMatch.replacePromotedPiece(type);
+					
+			}
 		 }
 		catch(InputMismatchException e){
 			System.out.print(e.getMessage());
 			sc.nextLine();
 		}
 		catch(ChessException e) {
+			System.out.print(e.getMessage());
+			sc.nextLine();
+		}
+		catch(InvalidParameterException e) {
 			System.out.print(e.getMessage());
 			sc.nextLine();
 		}
